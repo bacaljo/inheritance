daa13a288b9fd431f28644bee071a18956fe133d
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-06 10:50:34 +0800

    Create tiger class inherited from felidae class

---
+++ src/objects/animal/felidae/Tiger.java
@@ -0,0 +1,9 @@
+package objects.animal.felidae;
+
+import objects.animal.Felidae;
+
+public class Tiger extends Felidae {
+    public Tiger(int age, int weight) {
+        super(age, weight);
+    }
+}
